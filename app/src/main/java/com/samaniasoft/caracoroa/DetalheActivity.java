81ebec694be9412c41884b7285f0e86ede5e256f
Samuel Teles <samuel.silva@uni9.edu.br>
2020-03-21 01:24:40 -0300

    Chamando Activity 1 para Activity 2

--- app/src/main/java/com/samaniasoft/caracoroa/DetalheActivity.java
+++ app/src/main/java/com/samaniasoft/caracoroa/DetalheActivity.java
@@ -1,14 +1,49 @@
 package com.samaniasoft.caracoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DetalheActivity extends AppCompatActivity {
+    private ImageView imgMoeda;
+    private ImageView btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhe);
+
+        imgMoeda = (ImageView) findViewById(R.id.moedaId); //Estou pegando o id
+        btnVoltar = (ImageView) findViewById(R.id.btnVoltarId);
+
+        Bundle extra = getIntent().getExtras(); // Estou pegando o Intent e o Extra da classe main
+
+        if (extra != null){
+            String opcapEscolhida = extra.getString("opcao"); //Pegando a key
+            if (opcapEscolhida.equals("cara")){
+                imgMoeda.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_cara)); //pegando a img do Drawable
+            }else{
+                imgMoeda.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_coroa));
+            }
+
+        }
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DetalheActivity.this, MainActivity.class)); //Voltando para outra activity
+            }
+        });
+
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/samaniasoft/caracoroa/MainActivity.java
+++ app/src/main/java/com/samaniasoft/caracoroa/MainActivity.java
@@ -2,13 +2,38 @@ package com.samaniasoft.caracoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView botaoJogar;
+    private String [] opcao = {"cara", "coroa"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoJogar = (ImageView) findViewById(R.id.btnJogarId);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(2);
+
+                //Intent usado para solicitar uma ação de outro componente do aplicativo
+                Intent intent = new Intent(MainActivity.this, DetalheActivity.class);
+                intent.putExtra("opcao", opcao[numeroAleatorio]);
+
+                startActivity(intent);
+
+            }
+        });
     }
 }
